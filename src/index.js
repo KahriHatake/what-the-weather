9b064c24bf80575675f8730cf2ec97471e12f61b
Kari Aguilar <aguilar.kari@icloud.com>
2025-07-23 00:30:06 -0700

    added axios and api
    still getting api key invalid with console log. Will continue to see if I can correct

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+function searchCity(city) {
+let apiKey = "4b08f845751300d9odtfbffbbf0216ac";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
-
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
