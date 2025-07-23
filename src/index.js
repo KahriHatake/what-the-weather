e3523bee1d0d9f9437c57c3791b2b83bf6b8fb0d
Kari Aguilar <aguilar.kari@icloud.com>
2025-07-22 22:55:37 -0700

    updated my file and rewrote code after troubleshooting.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-  }
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
-});
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
